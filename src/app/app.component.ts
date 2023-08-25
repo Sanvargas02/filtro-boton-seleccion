6f5e6cbad3c7969c4f9f86fa1162d369f077a286
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-25 15:08:31 -0500

    Filtro por Seleccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'filterPipe';
+  //Arreglo de datos traidos desde el back o Firebase
+  items: any[] = [
+    { nombre: 'Manzana', categoria: 'frutas' },
+    { nombre: 'Zanahoria', categoria: 'verduras' },
+    { nombre: 'Banana', categoria: 'frutas' },
+    { nombre: 'Lechuga', categoria: 'verduras' }
+  ];
+  //filteredItems es para recorrer en el html
+  filteredItems: any[] = this.items;
+
+  applyFilter(selectedCategory: any) {
+    let value = selectedCategory.target.value; // Obtenemos el valor
+    if (value === 'todos') {
+      this.filteredItems = this.items;
+    } else {
+      this.filteredItems = this.items.filter(item => item.categoria === value);
+    }
+  }
+
 }
